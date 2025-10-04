62e22d8f48ba60cf725fff7323f325b57a691cb1
sayan-hue <sayanghanta57@gmail.com>
2025-10-04 23:08:56 +0530

    adding dropdown for selecting languages

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const loginout = document.querySelector('.login-out');
+const userloginout = document.querySelector('.user')
 
 loginout.addEventListener('click', function(){
+    userloginout.classList.remove('active')
+    userloginout.classList.add('active')
     window.location.replace("login.html")
 })
