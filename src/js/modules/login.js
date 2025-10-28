5762b541eca256642324c3264e7dd4ffced27d0b
sayan-hue <sayanghanta57@gmail.com>
2025-10-29 00:02:58 +0530

    change made

--- src/js/modules/login.js
+++ src/js/modules/login.js
@@ -22,6 +22,6 @@ window.addEventListener('DOMContentLoaded', function(){
     });
 
     loginbutton.addEventListener('click', function(){
-        window.location.href = "http://127.0.0.1:5500/login.html";
+        window.location.href = "login.html";
     });
 });
